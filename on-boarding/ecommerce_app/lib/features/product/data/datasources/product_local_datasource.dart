6cb3586a409f4479488ca8fd8e17d4ad33cb837e
tsitiii <ziontaa9@gmail.com>
2025-08-04 19:46:16 +0300

    update on local data source implementation

--- on-boarding/ecommerce_app/lib/features/product/data/datasources/product_local_datasource.dart
+++ on-boarding/ecommerce_app/lib/features/product/data/datasources/product_local_datasource.dart
@@ -1,10 +1,26 @@
+import 'dart:convert';
 
+import 'package:mockito/mockito.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import '../../../../core/error/exceptions.dart';
 import '../../domain/entities/product.dart';
+import '../models/product_model.dart';
 
 class ProductLocalDatasource {
-  Product product;
-  ProductLocalDatasource({required this.product});
-  Future<void> cacheProducts(){
+  static const String CACHED_PRODUCTS_KEY = 'CACHED_PRODUCTS';
+  final SharedPreferences sharedPreferences;
 
+  ProductLocalDatasource({
+    required this.sharedPreferences
+  });
+
+  Future<void> cacheProducts(List<ProductModel> products) async{
+    try {
+      final jsonlist = products.map((product)=>product.toJson()).toList();
+      final jsonString = json.encode(jsonlist);
+      await sharedPreferences.setString(CACHED_PRODUCTS_KEY, jsonString);
+    } catch (e) {
+      throw CacheException();
   }
+}
 }
